77d89734c9dc0c04f9128c9bd37bf73f40e1eaab
FESSY <olaoluwaoni1@gmail.com>
2022-12-01 18:34:42 -0800

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from add_0 import add
+
+    from add_0.py import add
     a = 1
     b = 2
-    print(" {:d} + {:d} = {:d}".formart(a, b, add(a, b)))
+
+    print(f"{a:d} + {b:d} = {add(a, b)}")
