8dc99d29e8cd1550bc57e265a63cf087918d1e28
FESSY <olaoluwaoni1@gmail.com>
2022-12-21 05:45:30 -0800

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value), end="")
-        return True
+        return (True)
     except TypeError:
-        return false
+        return (false)
